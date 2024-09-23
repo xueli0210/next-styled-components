bac143aa1afdcfcbab482635da947ff513c0be4d
Xue Li <xuelimiis@gmail.com>
2024-09-23 13:36:34 -0700

    [prep] MainWrapper: single out the client component

---
+++ src/components/MainWrapper/MainWrapper.js
@@ -0,0 +1,14 @@
+"use client";
+import styled from "styled-components";
+
+const MainWrapper = styled.main`
+  width: 100%;
+  max-width: 800px;
+  padding: 16px 24px;
+  margin: 0px auto;
+  border: 1px solid hsl(0deg 0% 50% / 0.3);
+  border-radius: 2px;
+  background: white;
+`;
+
+export default MainWrapper;
